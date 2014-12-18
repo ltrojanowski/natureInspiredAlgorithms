616e97e3c2b56be878862ab0897da859c3570c7d
LukaszTrojanowski <luk.trojanowski@gmail.com>
2014-12-18 19:24:22 +0100

    working GA without visualisation

--- AlgE-1/app.R
+++ AlgE-1/app.R
@@ -1,96 +1,166 @@
 library("random")
 #library("foreach")
 
-testfunction <- function()
-{
-  teststring <- "010101010101"
-}
-
 onemax <- function(bitstring){
-  bitstring_split <- strsplit(bitstring, "")[[1]]
-  sum <- 0
-  for(bit in bitstring_split){
-    if (bit == "1")
-    {
-      sum <- sum + 1
-    }
-  }
-  return(sum)
+  sum(bitstring)
 }
 
 random_bitstring <- function(num_bits)
 {
-  random_bitstring <- randomNumbers(num_bits, min = 0, max = 1, col = num_bits)  #not correct, need to define size as vec and join.
-  return(paste(random_bitstring, collapse = ''))
+  rand_bitstring <- sample(c(0,1), num_bits, replace = TRUE, prob = NULL)
 }
 
 binary_tournament <- function(pop){
-  ij <- sample(1:length(pop), 2, raplace = F)
-  i = ij[1]
-  j = ij[2]
-  (pop[i])
+  indexes <- sample(1:ncol(pop), 2, replace = FALSE)
+  #comment out the next two lines after testing
+  #cat('\nbinary vec one: ', pop[,indexes[1]])
+  #cat('\nbinary vec two: ', pop[,indexes[2]])
+  if (onemax(pop[,indexes[1]])>=onemax(pop[,indexes[2]])) return(pop[,indexes[1]]) else return(pop[,indexes[2]])
 }
 
-
-
-point_mutation <- function(bitstring)
+point_mutation <- function(bitstring, rate=1.0/length(bitstring))
 {
-  bitstring_size <- size(bitstring_split <- strsplit(bitstring,"")[[1]])
-  rate <- 1.0/bitstring_size
+  bitstring_size <- length(bitstring)
+  #rate <- 1.0/bitstring_size
   randvec <- runif(bitstring_size, 0.0, 1.0)
-  index <- 1
-  for(i in seq_along(randvec)) {
-    if (randvec[[i]] == 1)
-    {
-      if (bitsring_split[[i]] == "1")
-        bitstring_split[[i]] = "0"
-      else
-        bitstring_split[[i]] = "1"
-    }
-  }
-  bitstring <- paste(bitstring_split, collapse="")
-  return(bitstring)
+  bitstring <- ifelse(randvec < rate, ifelse(bitstring == 1, 0, 1), bitstring)
 }
 
 crossover <- function(parent1, parent2, rate)
 {
-  if (runif(1, 0.0, 1.0) >= rate)
+  if (runif(1, 0.0, 1.0) > rate)
   {
-    return(parent1)
+    toReturn <- parent1
   } else {
-    crossoverPoint = sample(1:length(parent1), 1)
-    toReturn <- paste(parent1[1:crossoverPoint],parent2[crossoverPoint+1:length(parent2)],sep="")
-    return toReturn    
+    crossoverPoint = sample(2:length(parent1)-1, 1)
+    toReturn <- c(c(parent1[1:crossoverPoint]),c(parent2[(crossoverPoint+1):length(parent2)]))   
   }
+  toReturn
 }
 
 reproduce <- function(selected, pop_size, p_cross, p_mutation){
-  children <- vector()
-  lasindex <- length(selected)
-  for (index in seq_along(selected))
+  children <- matrix(nrow = nrow(selected), ncol = pop_size)#nrow = nrow(selected), ncol = pop_size)
+  indexes <- rep(1:ncol(selected), length=pop_size)
+  counter <- 1
+  for (index in indexes)
   {
-    p1 <- selected[index]
-    (index %% 2 == 1) ? (p2 <- selected[index+1]) : (p2 <- selected[index - 1])
-    if (index == length(selected) && index %% 2 == 1)
-      p2 <- selected[index-1]
+    p1 <- selected[,index]
+    if (index+1 > ncol(selected)) {
+      p2 <- selected[,1]
+    } else if(index %% 2 == 1){
+      p2 <- selected[,index+1]
+    } else {
+      p2 <- selected[,index-1]
+    }
+    #if (is.na(p2) == TRUE)
+    #  p2 <- selected[,1]
     child <- crossover(p1, p2, p_cross)
     child <- point_mutation(child, p_mutation)
-    append(children, child)
+    children[, counter] <- child
+    counter <- counter + 1
   }
-  return children
+  return(children)
 }
 
 
 search <- function(max_genes, num_bits, pop_size, p_crossover, p_mutation){
-  bitstrings <- list()
+  #initialization population
+  population <- matrix(ncol=pop_size, nrow=num_bits)
   for (index in 1:pop_size){
-    bitstrings[index] <- random_bitstring(num_bits)
+    population[,index] <- random_bitstring(num_bits)
   }
-  fitness <- array()
-  for (index in seq_along(pop_size)){
-    fitness[index] <- onemax(bitstring[index])
+  #compute fitness
+  fitness <- vector(length=pop_size)
+  for (index in 1:pop_size){
+    fitness[index] <- onemax(population[,index])
   }
-  population <- cbind(population, fitness)
-  
-  
+  #
+  #sorted_indices <- vector(length=pop_size)
+  sorted_indices <- unname(unlist(sort(fitness, index.return = TRUE)['ix']))
+  best <- fitness[sorted_indices[1]]
+  best_bitstring <- population[sorted_indices[1]]
+  cat('\ninitial best: ', best)
+  selected <- matrix(ncol=pop_size, nrow=num_bits)
+  for(gen in 1:max_genes){
+    for(index in 1:pop_size){
+      selected[,index] <- binary_tournament(population)
+    }
+    children <- reproduce(selected, pop_size, p_crossover, p_mutation)
+    for (index in 1:pop_size){
+      fitness[index] <- onemax(children[,index])
+    }
+    sorted_indices <- unname(unlist(sort(fitness, index.return = TRUE)['ix']))
+    if (fitness[sorted_indices[1]] >= best){
+      best <- fitness[sorted_indices[1]]
+      population <- children
+      cat('\ngen: ', gen, 'best: ', best, 'bitstring: ', population[,sorted_indices[1]])
+    }
+    #else {
+    #  cat('\nbest: ', best, 'bitstring: ', selected[,1])
+    #}
+    if (best == num_bits) break;
+  }
+  best
 }
+
+test <- function() {
+  # test of onemax expected result 10
+  print('test of onemax, expected result vector of 10')
+  test1 <- onemax(c(1,1,1,1,1,1,1,1,1,1))
+  cat('value: ', test1)
+  cat('\nclass: ', class(test1))
+  # test of random_bitstring
+  print('test of random_bitstring, expected vector of 10 random values ')
+  test2 <- random_bitstring(10)
+  cat('value: ', test2)
+  cat('\nclass: ', class(test2))
+  # test of binary_tournament
+  #generate example matrix
+  cat('\ntest of binary_tournament')
+  number <- 20
+  examplematrix <- matrix(nrow=10, ncol = number)
+  for(index in 1:number) {
+    examplematrix[,index] <- random_bitstring(10);
+  }
+  selected <- binary_tournament(examplematrix)
+  cat('\nvalue: ', selected)
+  cat('\nclass:', class(selected))
+  #test of pointmutation
+  cat('\ntest of point_mutation expect different in one prob one point of all ones')
+  mutated_bitstring <- point_mutation(c(1,1,1,1,1,1,1,1,1,1))
+  cat('\nvalue: ', mutated_bitstring)
+  cat('\nclass: ', class(mutated_bitstring))
+  #test crossover
+  cat('\ntest of crossover')
+  crossed_bitstring <- crossover(c(1,1,1,1,1,1,1,1,1,1), c(0,0,0,0,0,0,0,0,0,0), 1)
+  cat('\nvalue: ', crossed_bitstring)
+  cat('\nclass: ', class(crossed_bitstring))
+  #test reproduce
+  cat('\ntest of reproduce')
+  test_pop<-matrix(ncol=15, nrow=10)
+  for (i in 1:ncol(test_pop)){
+    test_pop[,i] <- random_bitstring(nrow(test_pop))
+  }
+  cat('\n test population matrix:\n', test_pop)
+  cat('\n class of test population:', class(test_pop), 'dim:', dim(test_pop))
+  test_child_pop <- reproduce(test_pop, ncol(test_pop), 0.98, 0.1)
+  cat('\nclass of test_child_pop', class(test_child_pop))
+  cat("\n test child pop:\n", test_child_pop)
+  cat('\n dimensions of test child pop: ', dim(test_child_pop))
+}
+
+
+run <- function(){
+  #problem configuration
+  num_bits <- 64
+  # algorithm configuration
+  max_genes <- 300
+  pop_size <- 100
+  p_crossover <- 0.98
+  p_mutation <- 1.0/num_bits
+  #execute the algorithm
+  best <- search(max_genes, num_bits, pop_size, p_crossover, p_mutation)
+  cat('\n Done! Solution: ', best)
+}
+
+run();
